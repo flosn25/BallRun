cd78bcb8a1b68145eaabd67b3afafb2691d270a3
Florian Lange <florian.lange@mindbox.de>
2014-09-17 12:05:17 +0200

    init commit

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	public GameObject Player;
+	public int y= 1;
+	public int z = 3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {/*übernimmt KameraPosition , bleibt leicht dahinter*/
+		transform.position = new Vector3(Player.transform.position.x, y, Player.transform.position.z-z);
+	}
+}
---
+++ Assets/Touch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+----------------------------------------------------------------
+Event-Handler zum Spielabbruch.
+Sobald touch erfolgt, kommt Ende
+----------------------------------------------------------------
+ */
+public class Touch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision col){
+		print ("you can't swim!");
+	}
+}
