b24961fa71974bf2a5a56d3d80a7cd90b5266291
Max <maximilianheyne@googlemail.com>
2014-09-24 13:19:18 +0200

    neue Oberfläche oben links, Coins als Powerup, Animation

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour {
+	/*/////////////////////////////////////
+	PowerUp benötigt:
+		SphereCollider als Trigger!
+		Animation, + beide PowerUp Scripte
+	 //////////////////////////////////////*/
+
+	void OnTriggerEnter (Collider col){
+		//Debug.Log (this.gameObject); // gibt angestoßenes Objekt aus
+		//this.gameObject.renderer.enabled = false; //nicht möglich als Entfernen, da Objekt weiterhin im Weg 
+		StartCoroutine ("PlayAnimation");//Startet IEnumerator
+	}
+
+	IEnumerator PlayAnimation()
+	{
+		transform.animation.Play("PowerUp");//SPielt Animation PowerUp ab
+		yield return new WaitForSeconds(1);//wartet 1 Sekunde, so dass Animation ablaufen kann
+		Destroy(this.gameObject);//"zerstört" PowerUp
+	}
+}
--- Assets/PowerUp.js
+++ Assets/PowerUp.js
@@ -3,7 +3,7 @@ var Rotation : float = 35.0; //Geschwindigkeit der Rotation
 function Start () {
 
 }
-function Update ()
+function Update ()//Standard-Animation von PowerUps	
 {
 	transform.Rotate (Rotation * Time.deltaTime, 0.0, 0.0); //Time.deltaTime für ständige Rotation
 }
