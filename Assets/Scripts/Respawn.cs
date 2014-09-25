b28dfba1711e3d60d21322b805fbd5480ea95f04
Max <maximilianheyne@googlemail.com>
2014-09-25 18:02:15 +0200

    Ball leicht geändert (sinkt nicht mehr ein).

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void respawn(){
+		Debug.Log ("Nein, Nein, Nein");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
