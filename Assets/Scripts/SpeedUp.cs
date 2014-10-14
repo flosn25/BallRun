723760034cc69cc0515e2e3a86dd33036b8aa5c4
Christoph Broeckmann <christoph.broeckmann@gmail.com>
2014-10-14 17:53:02 +0200

    Dateien hinzugefügt (hat davor im Sourcetree irgendwie nicht geklappt)

---
+++ Assets/Scripts/SpeedUp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpeedUp : MonoBehaviour {
+
+	public float speedUpFactor = 5; // Faktor, um den timeScale erhöht wird
+	private float prevSpeed; // Geschwindigkeit bevor Ball über Feld
+
+	// Use this for initialization
+	void Start() {
+	}
+
+	// Wenn Ball das Feld betritt
+	void OnCollisionEnter(Collision collision) {
+		prevSpeed = Time.timeScale; // vorherige Geschwindigkeit zwischenspeichern
+		Time.timeScale *= speedUpFactor; // Geschwindigkeit mit Faktor multiplizieren
+		//Debug.Log ("OnCollisionEnter: Player is above me");
+	}
+
+	// Wenn Ball das Feld wieder verlässt
+	void OnCollisionExit(Collision collision) {
+		Time.timeScale = prevSpeed; // vorherige Geschwindigkeit wieder herstellen
+		//Debug.Log ("OnCollisionExit: Player is not above me any more");
+	}
+
+}
