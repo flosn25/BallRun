d6cc6605b3ebd19fd197aff74cabc6499ad918dc
Jens Schulte <jensschulte1@gmail.com>
2014-10-14 18:35:11 +0200

    level angepasst

---
+++ Assets/Scripts/JumpPower.js
@@ -0,0 +1,8 @@
+﻿var Rotation : float = 35.0; //Geschwindigkeit der Rotation
+function Start () {
+
+}
+function Update ()//Standard-Animation von PowerUps	
+{
+	transform.Rotate (0.0,Rotation * Time.deltaTime, 0.0); //Time.deltaTime für ständige Rotation
+}
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PowerUp : MonoBehaviour {
 	/*/////////////////////////////////////
@@ -8,26 +9,25 @@ public class PowerUp : MonoBehaviour {
 		Animation, + beide PowerUp Scripte
 	 //////////////////////////////////////*/
 
-			public Player player;
+	public Player player;
+	public Vector3 jumpVelocity;
+	public bool touch;
 	
 	void Start() {
-		GameObject playerObject = GameObject.Find ("Sphere");
-		player = playerObject.GetComponent<Player> ();
+		GameObject playerObject = GameObject.Find ("Sphere");//
+		player = playerObject.GetComponent<Player> ();//
 	}
+
 	void OnTriggerEnter (Collider col){
-		
-		//Debug.Log (this.gameObject); // gibt angestoßenes Objekt aus
-		//this.gameObject.renderer.enabled = false; //nicht möglich als Entfernen, da Objekt weiterhin im Weg 
-		//StartCoroutine ("PlayAnimation");//Startet 
-		player.points = player.points + 1000;
-		Destroy(this.gameObject);
+		player.points = player.points + 1000;//Puntke bei einsammeln 1000 hochzählen
+		var Punkte = GameObject.Find ("Text_Points").GetComponent<Text> ();//Angezeigten Text finden
+		Punkte.text = player.points.ToString();//Anzeige von Texten
+		Destroy(this.gameObject);//PowerUp auflösen
+
+		if (collider.gameObject.name.Equals("pfeilmax")) //ob Sprung-PowerUp
+		{
+			player.rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);//"Sprung" durch AddForce, JumpVelocity per Hand nachgestellt (0,10,0)
+			touch = false;// erst wieder Sprung, wenn Collision auf true
+		}
 	}
-
-
-	/*IEnumerator PlayAnimation()
-	{
-		//transform.animation.Play("PowerUp");//SPielt Animation PowerUp ab
-		//yield return new WaitForSeconds(1);//wartet 1 Sekunde, so dass Animation ablaufen kann
-		Destroy(this.gameObject);//löst PowerUp auf PowerUp
-	}*/
 }
